c6de30747c6a23e83c379925b222589041462729
Ederam <53803895+Ederam@users.noreply.github.com>
2025-02-25 16:58:17 -0500

    Creacion de plantilla inicial

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios Crud</title>
+</head>
+<body>
+    <div>
+        <form action="">
+            <h1>Crear usuario</h1>
+
+            <input type="text" name="name" placeholder="Nombre">
+            <input type="text" name="lastname" placeholder="Apellido">
+            <input type="text" name="username" placeholder="Username">
+            <input type="text" name="password" placeholder="Password">
+            <input type="text" name="email" placeholder="Email">
+
+            <input type="submit" value="Agrega Usuario">
+        </form>
+    </div>
+
+    <div class="users-table">
+        <h2>Usuarios registrados</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellidos</th>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>Email</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- <?php while ($row = mysqli_fetch_array($query)): ?> -->
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+
+                        <th><a href="Editar"></a></th>
+                        <th><a href="Eliminar"></a></th>
+
+                        <!-- <th><?= $row['id'] ?></th>
+                        <th><?= $row['name'] ?></th>
+                        <th><?= $row['lastname'] ?></th>
+                        <th><?= $row['username'] ?></th>
+                        <th><?= $row['password'] ?></th>
+                        <th><?= $row['email'] ?></th>
+                        <th><a href="update.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></th>
+                        <th><a href="delete_user.php?id=<?= $row['id'] ?>" class="users-table--delete" >Eliminar</a></th> -->
+                    </tr>
+                <!-- <?php endwhile; ?> -->
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
